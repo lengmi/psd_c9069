7a8d46c4d9905349484021608de34783dc270111
Joe Occam <zzhong@hust.edu.cn>
2015-11-23 11:21:01 +0800

    Add variance calculation.

--- DataAcquire.py
+++ DataAcquire.py
@@ -3,13 +3,14 @@
 import sys
 import time
 import serial
+import statistics
 
 ser = serial.Serial("/dev/ttyUSB0",38400)
 
 def getData(ser, amount):
 	data = []
 	while amount != 0:
-		ser.flush()
+		ser.flushInput()
 		tmp = ser.readline()
 		if(len(tmp) == 10):
 			amount -= 1
@@ -24,7 +25,8 @@ def countXYZ(data):
 		x.append(float(int(tmp[0:3],16))*12.0/4096.0)
 		y.append(float(int(tmp[3:6],16))*14.0/4096.0)
 		z.append(float(int(tmp[6:9],16))*36.0/4096.0)
-	return [sum(x)/len(x),sum(y)/len(y),sum(z)/len(z)]
+	return [6.0-sum(x)/len(x),7.0-sum(y)/len(y),sum(z)/len(z),\
+	statistics.variance(x),statistics.variance(y),statistics.variance(z)]
 
 while True:
 	data = getData(ser,100)
